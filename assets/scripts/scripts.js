3e2a623af84aff3ccef5fbb3e109312dbf608d90
Timothy <thuang87@berkeley.edu>
2018-04-15 08:14:14 -0700

    awefoij

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,8 +8,35 @@ $(document).ready(function() {
         setTimeout(function(){
             $(".companyName").append(companyName);
             $(".companyName").fadeIn();
-
+            $(".chart-container").fadeIn();
+            var myChart = new Chart(ctx, {
+              type: 'bar',
+              data: {
+                labels: labels,
+                datasets: [
+                  {
+                    data: values,
+                    backgroundColor: [
+                            'white',
+                            'white',
+                            'white',
+                            'white'
+                        ],
+                    label: 'Most Valued Traits',
+                    fill: false
+                  }
+                ]
+              }
+            });
         }, 600);
 
     });
 });
+
+Chart.defaults.global.defaultFontColor = 'white';
+Chart.defaults.global.defaultFontSize = 25;
+Chart.defaults.global.defaultFontFamily = 'Abel';
+var labels = ['extroversion', 'curiosity', 'self-will', 'dumbass']
+var values = [30.9, 94.2, 93.9, 89.5];
+
+var ctx = document.getElementById("myChart");
